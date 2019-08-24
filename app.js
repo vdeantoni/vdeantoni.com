898f9787ac5e9d5ba4314b2e49a34d06c8ea4dde
Vinicius De Antoni <vdeantoni@blizzard.com>
2019-08-23 21:47:36 -0700

    Removed old files

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var compression = require('compression');
-
-var app = express();
-
-app.use(compression());
-app.use('/', express.static(__dirname + '/dist'));
-
-app.listen(80);
