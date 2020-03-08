e684927d95a2432ebf56793b57501366d2123847
Vinicius De Antoni <vdeantoni@gmail.com>
2020-03-08 13:59:46 -0700

    Tailwindcss setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1 className="hover:bg-black my-16 text-red-500">Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `500px`, marginBottom: `1.45rem` }}>
