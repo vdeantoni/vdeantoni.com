cae456d35643fd7460ead7977d8c43a27615f2ac
Vinicius De Antoni <vdeantoni@gmail.com>
2020-03-22 15:22:28 -0700

    Merged home and resume into index

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,10 +6,10 @@
  */
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { graphql, Link, useStaticQuery } from "gatsby"
-import React, { useState } from "react"
-import Logo from "../assets/svgs/logo.svg"
+import { graphql, useStaticQuery } from "gatsby"
 import { OutboundLink } from "gatsby-plugin-google-analytics"
+import React from "react"
+import Header from "./header"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -17,10 +17,6 @@ const Layout = ({ children }) => {
       site {
         siteMetadata {
           title
-          menuLinks {
-            name
-            path
-          }
           contactLinks {
             name
             link
@@ -31,81 +27,13 @@ const Layout = ({ children }) => {
     }
   `)
 
-  const [open, setOpen] = useState(false)
-
   return (
     <>
-      <header className="bg-gray-800 sticky top-0 z-1">
-        <div className="max-w-7xl mx-auto flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center">
-            <Logo className="h-8 w-8 mr-4" />
-            <h1 className="ty-h5 text-white text-shadow">
-              {data.site.siteMetadata.title}
-            </h1>
-          </div>
-          <nav className="hidden md:flex">
-            <div className="ml-10 flex items-baseline">
-              {data.site.siteMetadata.menuLinks.map(link => (
-                <Link
-                  key={link.name}
-                  to={link.path}
-                  className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                  activeClassName="text-white bg-gray-900"
-                >
-                  {link.name}
-                </Link>
-              ))}
-            </div>
-          </nav>
-          <div className="-mr-2 flex md:hidden">
-            <button
-              onClick={() => setOpen(!open)}
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
-              aria-label="Toggle Menu"
-            >
-              <svg
-                className="h-6 w-6"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  className={`${open ? "hidden" : "inline-flex"}`}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-                <path
-                  className={`${open ? "inline-flex" : "hidden"}`}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </div>
-        </div>
-        <nav className={`${open ? "" : "hidden"} md:hidden`}>
-          <div className="px-2 pt-2 pb-3 sm:px-3">
-            {data.site.siteMetadata.menuLinks.map(link => (
-              <Link
-                key={link.name}
-                to={link.path}
-                className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                activeClassName="text-white bg-gray-900"
-              >
-                {link.name}
-              </Link>
-            ))}
-          </div>
-        </nav>
-      </header>
+      <Header title={data.site.siteMetadata.title} />
 
-      <main className="container mx-auto px-4 sm:px-6 lg:px-8">{children}</main>
+      <main>{children}</main>
 
-      <footer className="relative flex items-center justify-center bg-gray-200 h-16 mt-8 px-8">
+      <footer className="relative flex items-center justify-center bg-gray-200 h-16 px-8">
         <div className="flex flex-1 justify-center">
           {data.site.siteMetadata.contactLinks.map(link => (
             <OutboundLink
