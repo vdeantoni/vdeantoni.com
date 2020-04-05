727ccc6b2f3720894630801c4b95e531388e736e
Vinicius De Antoni <vdeantoni@gmail.com>
2020-04-04 20:37:47 -0700

    Added publications page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,75 @@
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 import React from "react"
+import { Blurb } from "../components/home/blurb"
 import Layout from "../components/layout"
+import { MediumCard } from "../components/publication/medium-card"
 import SEO from "../components/seo"
-import Home from "../components/home/home"
-import Resume from "../components/resume/resume"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
+export const query = graphql`
+  query {
+    image: file(name: { eq: "me" }) {
+      childImageSharp {
+        fluid(maxWidth: 350) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
 
-    <Home />
-    <Resume />
-  </Layout>
-)
+    latestPublication: allMediumPost(
+      sort: { fields: [createdAt], order: DESC }
+      limit: 1
+    ) {
+      nodes {
+        id
+        title
+        virtuals {
+          subtitle
+          previewImage {
+            imageId
+          }
+          tags {
+            name
+          }
+          readingTime
+        }
+        createdAt
+        uniqueSlug
+      }
+    }
+  }
+`
+
+const IndexPage = ({ data }) => {
+  const { image, latestPublication } = data
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+
+      <section>
+        <div className="container min-h-main-screen flex items-center justify-center flex-col">
+          <div className="md:flex items-center  md:flex-row-reverse">
+            <div className="md:flex-1 flex justify-center md:pt-16">
+              <Img
+                fluid={image.childImageSharp.fluid}
+                className="rounded-full m-8 max-w-xs w-full"
+                alt="Vinicius De Antoni"
+              ></Img>
+            </div>
+            <div className="md:flex-1 ty-rich-text text-justify">
+              <Blurb></Blurb>
+            </div>
+          </div>
+          <div className="self-start my-8">
+            <h2 className="ty-h5 mb-4">Latest publication</h2>
+
+            <MediumCard post={latestPublication.nodes[0]}></MediumCard>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  )
+}
 
 export default IndexPage
