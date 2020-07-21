a83b97d546f0b10ca439a227f89b2746cccbd919
Vinicius De Antoni <vdeantoni@gmail.com>
2020-07-21 11:48:52 -0700

    Decreased background points opacity

--- src/hooks/use-request-animation-frame.js
+++ src/hooks/use-request-animation-frame.js
@@ -14,6 +14,7 @@ const useRequestAnimationFrame = (callback, deps = []) => {
     return () => {
       cancelAnimationFrame(ref.current);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, deps);
 };
 
