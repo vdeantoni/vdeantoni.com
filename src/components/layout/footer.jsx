fe920e204feb6692b57ec000228a12583229089a
Vinicius De Antoni <vdeantoni@gmail.com>
2020-07-25 11:44:50 -0700

    Added open source disclaimer to footer

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -2,6 +2,7 @@ import * as $ from "classnames";
 import React from "react";
 import NavLinks from "./nav-links.jsx";
 import SocialLinks from "./social-links.jsx";
+import { OutboundLink } from "gatsby-plugin-google-analytics";
 
 const Footer = () => {
   return (
@@ -10,8 +11,27 @@ const Footer = () => {
         <NavLinks footer={true} className={$("gap-4", "my-6", "md:my-0", "md:hidden")} />
         <SocialLinks />
       </div>
-      <div className={$("border-t", "text-center", "py-4", "md:py-5", "md:py-6", "mt-4", "md:mt-5", "md:mt-6")}>
-        <p className={$("text-sm")}>© {new Date().getFullYear()}</p>
+      <div
+        className={$(
+          "border-t",
+          "text-sm",
+          "flex",
+          "justify-between",
+          "py-4",
+          "md:py-5",
+          "md:py-6",
+          "mt-4",
+          "md:mt-5",
+          "md:mt-6"
+        )}
+      >
+        <div className={$("text-sm")}>
+          This site is{" "}
+          <OutboundLink href="https://github.com/vdeantoni/vdeantoni.com" target="_blank" rel="noopener noreferrer">
+            open source.
+          </OutboundLink>
+        </div>
+        <div className={$("text-sm", "self-end")}>© {new Date().getFullYear()}</div>
       </div>
     </footer>
   );
