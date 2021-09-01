8cdec14cdc261a048253370e21ffc662e1d5e6cd
Vinicius De Antoni <vdeantoni@gmail.com>
2021-09-01 15:08:07 -0700

    Updated tests

--- cypress/integration/nav.spec.js
+++ cypress/integration/nav.spec.js
@@ -25,22 +25,28 @@ describe("Navigation", () => {
         cy.visit("/");
         cy.get("nav.desktop a[title=Home]").should("exist");
         cy.get("nav.desktop a[title=Posts]").should("exist");
+        cy.get("nav.desktop a[title=Projects]").should("exist");
         cy.get("nav.desktop a[title=Resume]").should("exist");
       });
       it("should take you to the posts page", () => {
         cy.visit("/");
         cy.get("nav.desktop a[title=Posts]").click();
-        cy.path().should("eq", "/posts");
+        cy.path().should("match", /\/posts\/?/);
+      });
+      it("should take you to the projects page", () => {
+        cy.visit("/");
+        cy.get("nav.desktop a[title=Projects]").click();
+        cy.path().should("match", /\/projects\/?/);
       });
       it("should take you to the resume page", () => {
         cy.visit("/");
         cy.get("nav.desktop a[title=Resume]").click();
-        cy.path().should("eq", "/resume");
+        cy.path().should("match", /\/resume\/?/);
       });
-      it("should take you to the home page", () => {
+      it("should take you back to the home page", () => {
         cy.visit("/posts");
         cy.get("nav.desktop a[title=Home]").click();
-        cy.path().should("eq", "/");
+        cy.path().should("match", /\//);
       });
       it("should show which page is active", () => {
         cy.visit("/");
@@ -50,6 +56,10 @@ describe("Navigation", () => {
         cy.visit("/posts");
         cy.get("nav.desktop a.active").should("have.length", "1");
         cy.get("nav.desktop a[title=Posts]").should("have.class", "active");
+
+        cy.visit("/projects");
+        cy.get("nav.desktop a.active").should("have.length", "1");
+        cy.get("nav.desktop a[title=Projects]").should("have.class", "active");
 
         cy.visit("/resume");
         cy.get("nav.desktop a.active").should("have.length", "1");
