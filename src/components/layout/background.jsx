e6e96f6b9f3311df4bf1ad187d8d4d78498b5887
Vinicius De Antoni <vdeantoni@gmail.com>
2021-11-23 00:51:13 -0800

    New background animation with threejs

--- src/components/layout/background.jsx
+++ src/components/layout/background.jsx
@@ -1,40 +1,75 @@
+import { Stars } from "@react-three/drei";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import cn from "classnames";
-import React, { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
+import { random, range } from "lodash";
+import React, { Suspense, useRef } from "react";
+import { Color, TextureLoader } from "three";
 import useColorScheme from "../../hooks/use-color-scheme";
-import useRequestAnimationFrame from "../../hooks/use-request-animation-frame";
-import Field from "../../lib/particles/field";
+import cloudTexture from "../../images/cloud.png";
+
+const fogColor = new Color(0x0d202d);
+const clouds = range(0, 100).map(() => ({
+  position: [random(-500, 500), random(-500, 500), random(-500, 500)],
+  rotation: [0, 0, Math.random() * 2 - Math.PI],
+}));
+
+const Cloud = (props) => {
+  const cloudMap = useLoader(TextureLoader, cloudTexture);
+  const mesh = useRef();
+
+  useFrame(() => {
+    mesh.current.rotation.z += 0.001;
+  });
+
+  return (
+    <mesh {...props} ref={mesh}>
+      <planeBufferGeometry args={[500, 500]} />
+      <meshLambertMaterial map={cloudMap} transparent={true} opacity={0.15} />
+    </mesh>
+  );
+};
 
 const Background = ({ className }) => {
   const [colorScheme] = useColorScheme();
 
-  const [field, setField] = useState(null);
-
-  const canvasRef = useRef(null);
-
-  useRequestAnimationFrame(() => {
-    if (!canvasRef.current) return;
-    if (!field) return;
-
-    canvasRef.current.getContext("2d").clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-
-    if (colorScheme !== "dark") return;
-
-    field.draw(canvasRef.current);
-    field.process();
-  }, [field, colorScheme]);
-
-  useEffect(() => {
-    const [w, h] = [window.innerWidth, window.innerHeight];
-    canvasRef.current.width = w;
-    canvasRef.current.height = h;
-
-    setField(new Field(w, h));
-  }, []);
+  if (colorScheme !== "dark") return null;
 
   return (
-    <div className={cn(className)}>
-      <canvas ref={canvasRef}></canvas>
-    </div>
+    <motion.div
+      className={cn(className)}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{
+        duration: 1,
+      }}
+    >
+      <Canvas
+        camera={{
+          fov: 60,
+          aspect: window.innerWidth / window.innerHeight,
+          near: 1,
+          far: 1000,
+          position: [0, 0, 500],
+        }}
+      >
+        <color attach="background" args={[fogColor]} />
+        <fogExp2 color={fogColor} density={0.001} />
+
+        <ambientLight color={0x555555} />
+        <pointLight color={0xcc6600} intensity={50} distance={450} decay={1.7} position={[-600, -250, 0]} />
+
+        <pointLight color={0x3677ac} intensity={50} distance={450} decay={1.7} position={[600, 250, 0]} />
+
+        <Suspense fallback={<></>}>
+          {clouds.map((props, n) => (
+            <Cloud key={n} {...props} />
+          ))}
+        </Suspense>
+
+        <Stars radius={140} depth={50} count={5000} factor={4} saturation={0} fade />
+      </Canvas>
+    </motion.div>
   );
 };
 
