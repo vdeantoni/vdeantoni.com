c56a9303e754d2b651af696ec41ed7a3c543886c
Vinicius De Antoni <vdeantoni@gmail.com>
2022-09-06 13:45:17 -0700

    Disabled SSR for ColorSchemeToggle component

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,16 +1,17 @@
 import { faBars } from "@fortawesome/free-solid-svg-icons/faBars";
-import { faMoon } from "@fortawesome/free-solid-svg-icons/faMoon";
-import { faSun } from "@fortawesome/free-solid-svg-icons/faSun";
 import { faTimes } from "@fortawesome/free-solid-svg-icons/faTimes";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import cn from "classnames";
-import { motion, useAnimation } from "framer-motion";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import Link from "next/link";
 import React, { FC, useState } from "react";
-import useColorScheme from "../../hooks/useColorScheme";
 import NavLinks from "./NavLinks";
 import SocialLinks from "./SocialLinks";
+
+const ColorSchemeToggle = dynamic(() => import("../ColorSchemeToggle"), {
+  ssr: false
+});
 
 const Branding: FC = () => {
   return (
@@ -49,41 +50,6 @@ const Branding: FC = () => {
         <span className={cn("hidden", "md:block", "ml-3")}>vdeantoni.com</span>
       </a>
     </Link>
-  );
-};
-
-const ColorSchemeToggle: FC<Partial<HTMLButtonElement>> = ({ className }) => {
-  const controls = useAnimation();
-  const [colorScheme, setColorScheme] = useColorScheme();
-
-  return (
-    <button
-      title="Toggle color mode"
-      onClick={async (e) => {
-        controls.stop();
-        e.currentTarget.blur();
-        await controls.start({
-          y: -20,
-          opacity: 0,
-          transition: { duration: 0.3 },
-          transitionEnd: { y: 10 }
-        });
-        setColorScheme(colorScheme === "default" ? "dark" : "default");
-        await controls.start({
-          y: 0,
-          opacity: 1,
-          transition: { duration: 0.5 }
-        });
-      }}
-      className={cn("a", "text-text", "overflow:hidden", className)}
-    >
-      <motion.div animate={controls}>
-        <FontAwesomeIcon
-          icon={colorScheme === "default" ? faSun : faMoon}
-          className={cn("w-5", "h-5")}
-        />
-      </motion.div>
-    </button>
   );
 };
 
